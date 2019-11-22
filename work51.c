fe38001bba2052e6dff8b2b073cff431498be8cb
SHADRIN MIKHAIL <55997913+SHADRINMMM@users.noreply.github.com>
2019-11-23 00:29:37 +0300

    Add files via upload

--- work51.c
+++ work51.c
@@ -11,35 +11,35 @@ void work51
   
   TH1D *cutflow1 = new TH1D("cutflow", "Number of accepted events", nbins, 1, nbins + 1);
   cutflow1->GetXaxis()->SetBinLabel(1, "All");
-  cutflow1->GetXaxis()->SetBinLabel(2, "");
-  cutflow1->GetXaxis()->SetBinLabel(3, "");
-  cutflow1->GetXaxis()->SetBinLabel(3, "");
+  cutflow1->GetXaxis()->SetBinLabel(2, "pt_ph_S > 150");
+  cutflow1->GetXaxis()->SetBinLabel(3, "pt_met_S> 130");
+  cutflow1->GetXaxis()->SetBinLabel(3, "N_jets_S>=2");
   
   TH1D *cutflow2 = new TH1D("cutflow", "Number of accepted events", nbins, 1, nbins + 1);
   cutflow2->GetXaxis()->SetBinLabel(1, "All");
-  cutflow2->GetXaxis()->SetBinLabel(2, "");
-  cutflow2->GetXaxis()->SetBinLabel(3, "");
-  cutflow2->GetXaxis()->SetBinLabel(3, "");
+  cutflow2->GetXaxis()->SetBinLabel(2, "pt_ph_S > 150");
+  cutflow2->GetXaxis()->SetBinLabel(3, "pt_met_S> 130");
+  cutflow2->GetXaxis()->SetBinLabel(3, "N_jets_S>=2");
   
-  TH1D *cutflow = new TH1D("cutflow", "Number of accepted events", nbins, 1, nbins + 1);
+  TH1D *cutfloww = new TH1D("cutflow", "Number of accepted events", nbins, 1, nbins + 1);
   cutflow->GetXaxis()->SetBinLabel(1, "All");
   TH1D *cutflow = new TH1D("cutflow", "Number of accepted events", nbins, 1, nbins + 1);
   cutfloww->GetXaxis()->SetBinLabel(1, "All");
 
   
-  TH1D *cutflow_S = new TH1D("cutflow", "Number of accepted events", nbins, 1, nbins + 1);
+  TH1D *cutflow_S = new TH1D("cutflow", "Number events with kriteria for signal", nbins, 1, nbins + 1);
   cutflow_S->GetXaxis()->SetBinLabel(1, "All");
-  cutflow_S->GetXaxis()->SetBinLabel(2, "");
-  cutflow_S->GetXaxis()->SetBinLabel(3, "");
-  cutflow_S->GetXaxis()->SetBinLabel(3, "");
-  cutflow_S->GetXaxis()->SetBinLabel(3, "");
+  cutflow_S->GetXaxis()->SetBinLabel(2, "mjj");
+  cutflow_S->GetXaxis()->SetBinLabel(3, "Rapidity");
+  cutflow_S->GetXaxis()->SetBinLabel(3, "centr");
+  cutflow_S->GetXaxis()->SetBinLabel(3, "pt balance");
   
-  TH1D *cutflow_B = new TH1D("cutflow", "Number of accepted events", nbins, 1, nbins + 1);
+  TH1D *cutflow_B = new TH1D("cutflow", "Number events with kriteria for background", nbins, 1, nbins + 1);
   cutflow_B->GetXaxis()->SetBinLabel(1, "All");
-  cutflow_B->GetXaxis()->SetBinLabel(2, "");
-  cutflow_B->GetXaxis()->SetBinLabel(3, "");
-  cutflow_B->GetXaxis()->SetBinLabel(3, "");
-  cutflow_B->GetXaxis()->SetBinLabel(3, "");
+  cutflow_B->GetXaxis()->SetBinLabel(2, "mjj");
+  cutflow_B->GetXaxis()->SetBinLabel(3, "Rapidity");
+  cutflow_B->GetXaxis()->SetBinLabel(3, "centr");
+  cutflow_B->GetXaxis()->SetBinLabel(3, "pt balance");
   
   
   TCanvas *c = new TCanvas("c","",1900,1900);
@@ -115,25 +115,25 @@ void work51
    tree2-> SetBranchAddress ( "jet_sublead_phi" , &phi_sublead_B);
    tree2-> SetBranchAddress ( "jet_sublead_E" , &E_sublead_B);
    
-   tree1-> SetBranchAddress ( "px" , &pt_ph_S);
-   tree1> SetBranchAddress ( "px" , &eta_ph_S);
-   tree1-> SetBranchAddress ( "px" , &phi_ph_S);
+   tree1-> SetBranchAddress ( "ph_pt" , &pt_ph_S);
+   tree1> SetBranchAddress ( "ph_eta" , &eta_ph_S);
+   tree1-> SetBranchAddress ( "ph_phi" , &phi_ph_S);
 
    
-   tree2-> SetBranchAddress ( "px" , &pt_ph_B);
-   tree2> SetBranchAddress ( "px" , &eta_ph_B);
-   tree2-> SetBranchAddress ( "px" , &phi_ph_B);
+   tree2-> SetBranchAddress ( "ph_pt" , &pt_ph_B);
+   tree2> SetBranchAddress ( "ph_eta" , &eta_ph_B);
+   tree2-> SetBranchAddress ( "ph_phi" , &phi_ph_B);
 
    
-   tree1-> SetBranchAddress ( "px" , &pt_met_S);
-   tree1-> SetBranchAddress ( "px" , &phi_met_S);
+   tree1-> SetBranchAddress ( "metTST_pt" , &pt_met_S);
+   tree1-> SetBranchAddress ( "metTST_phi" , &phi_met_S);
 
    
-   tree2-> SetBranchAddress ( "px" , &pt_met_B);
-   tree2-> SetBranchAddress ( "px" , &phi_met_B);
+   tree2-> SetBranchAddress ( "metTST_pt" , &pt_met_B);
+   tree2-> SetBranchAddress ( "metTST_phi" , &phi_met_B);
    
-   tree1-> SetBranchAddress ( "px" , &N_jets_S);
-   tree2-> SetBranchAddress ( "px" , &N_jets_B);
+   tree1-> SetBranchAddress ( "n_jet" , &N_jets_S);
+   tree2-> SetBranchAddress ( "n_jet" , &N_jets_B);
 
  Long64_t Kr_S=0;
  Long64_t Kr_cetr_S=0;
@@ -168,16 +168,21 @@ Long64_t Kr_pt_B =0;
 	met_S.SetPtEtaPhiM(pt_met_S,0,phi_met_S,0);
 //критерий на инвариантную массу 
 Kr_S=Kr_S+1;
+cutflow_S->Fill(1);
 if((lead_S.M()+sub_lead_S.M())>600) Kr_mass_S=Kr_mass_S+1;
+cutflow_S->Fill(2);
 //кртерий на разность быстрот
 if((abs(lead_S.Rapidity()-sub_lead_S.Rapidity()))>2.5) Kr_rapidity_S=Kr_rapidity_S+1;
+cutflow_S->Fill(3);
 //критерий на центральность 
 njj=(lead_S.PseudoRapidity()+sub_lead_S.PseudoRapidity())/2;	
 dnjj=(lead_S.PseudoRapidity()-lead_S.PseudoRapidity());
 zet=abs((ph_S.PseudoRapidity()-njj)/dnjj)
 if (zet<0.3) Kr_cetr_S=Kr_cetr_S+1;
+cutflow_S->Fill(4);
 //критерий на pt 
 if(pow(((lead_S+sub_lead_S+ph_S+met_S).Pt()),2)/(pow(lead_S.Pt(),2)+pow(sub_lead_S.Pt(),2)+pow(ph_S.Pt(),2)+pow(met_B.S(),2))<0.1) Kr_pt_S=Kr_pt_S+1;
+  cutflow_S->Fill(5);
   }
   
   
@@ -201,26 +206,57 @@ if(pow(((lead_S+sub_lead_S+ph_S+met_S).Pt()),2)/(pow(lead_S.Pt(),2)+pow(sub_lead
 	ph_B.SetPtEtaPhiM(pt_ph_B,eta_ph_B,phi_ph_B,0);
 	ph_B.SetPtEtaPhiM(pt_met_B,0,phi_met_B,0);
 //критерий на инвариантную массу 
+  cutflow_B->Fill(1);
 	Kr_B=Kr_B+1;
 if((lead_B.M()+sub_lead_B.M())>600) Kr_mass_B=Kr_mass_B+1;
+cutflow_B->Fill(2);
 //кртерий на разность быстрот
 if((abs(lead_B.Rapidity()-sub_lead_B.Rapidity()))>2.5) Kr_rapidity_B=Kr_rapidity_B+1;
+cutflow_B->Fill(3);
 //критерий на центральность 
 njj=(lead_B.PseudoRapidity()+sub_lead_B.PseudoRapidity())/2;	
 dnjj=(lead_B.PseudoRapidity()-lead_B.PseudoRapidity());
 zet=abs((ph_B.PseudoRapidity()-njj)/dnjj)
 if (zet<0.3) Kr_cetr_B=Kr_cetr_B+1;
+cutflow_B->Fill(4);
 //критерий на pt 
 if(pow(((lead_B+sub_lead_B+ph_B+met_B).Pt()),2)/(pow(lead_B.Pt(),2)+pow(sub_lead_B.Pt(),2)+pow(ph_B.Pt(),2)+pow(met_B.E(),2))<0.1) Kr_pt_B=Kr_pt_B+1;
+  cutflow_B->Fill(5);
   }
 Double_t Zn_Kr_mass =Kr_mass_S/sqrt(Kr_mass_S+Kr_mass_B);  
 Double_t Zn_Kr_rapidity =Kr_rapidity_S/sqrt(Kr_rapidity_S+Kr_rapidity_B);  ;  
 Double_t Zn_Kr_cetr =Kr_cetr_S/sqrt(Kr_cetr_S+Kr_cetr_B);  ;  
 Double_t Zn_Kr_pt =Kr_pt_S/sqrt(Kr_pt_S+Kr_pt_B);  ;  
  
-std::cout<<"значимость  "<< Zn_Kr_mass << std::endl;
-std::cout<<"значимость "<< Zn_Kr_rapidity << std::endl;
-std::cout<<"значимость "<<Zn_Kr_cetr << std::endl;
-std::cout<<"значимость "<< Zn_Kr_pt << std::endl; 
-  
+std::cout<<"значимость критерия для инвариантности масс "<< Zn_Kr_mass << std::endl;
+std::cout<<"значимость критерия для быстроты "<< Zn_Kr_rapidity << std::endl;
+std::cout<<"значимость критерия для центральности вылета "<<Zn_Kr_cetr << std::endl;
+std::cout<<"значимость критерия для баланса импульсов "<< Zn_Kr_pt << std::endl; 
+
+
+f1->Close();
+f2->Close();
+
+ Double_t scale1= 1/cutflow_S->Integral() ;
+ Double_t scale2= 1/cutflow_B->Integral() ;
+ Double_t scale3= 1/cutflow->Integral() ;
+ Double_t scale4= 1/cutflow1->Integral() ;
+ Double_t scale5= 1/cutfloww->Integral() ;
+ Double_t scale6= 1/cutflow2->Integral() ;
+ 
+ cutflow_S->Scale(scale1) ;
+ cutflow_B->Scale(scale2) ;
+ cutflow1->Scale(scale3) ;
+ cutflow2->Scale(scale5) ;
+ 
+ 
+ 
+ c->cd(1);
+  cutflow1->Draw("HIST");
+  cutflow2->SetLineColor(6);
+  cutflow2->Draw("HIST same");
+ c->cd(2);
+  cutflow_S->Draw("HIST");
+ c->cd(3);
+  cutflow_B->Draw("HIST");
 }
